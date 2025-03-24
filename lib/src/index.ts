4505a443a47758ca972b16600107f11409389524
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-03-24 21:14:05 +0530

    feat: create initial state with subscriptions

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,24 @@
-const foo = () => {
-    return "baz"
-}
+type Updater<State> = (state: State) => State;
+type Initializer<State> = (setState: (updater: Updater<State>) => void) => State;
+type Listener = () => void;
 
-export default foo;
+const create = <State>(initializer: Initializer<State>) => {
+
+    let state: State;
+    const listeners = new Set<Listener>();
+
+    const setState = (updater: Updater<State>) => {
+        const partiallyUpdatedState = updater(state);
+        if (partiallyUpdatedState !== state) { // TODO: improve this comparison
+            state = { ...state, ...partiallyUpdatedState };
+            listeners.forEach((listener) => listener());
+        }
+    }
+
+    const subscribe = (callback: () => void) => {
+        listeners.add(callback);
+        return () => listeners.delete(callback);
+    }
+
+    state = initializer(setState);
+}
