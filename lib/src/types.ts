6709170bf1f12eb1c5391ca66c1c991338f794cb
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-03-28 21:28:48 +0530

    feat: support mutable actions

--- lib/src/types.ts
+++ lib/src/types.ts
@@ -1,6 +1,8 @@
-type Updater<State> = (state: State) => Partial<State>;
+type NonMutatingAction<State> = (state: State) => Partial<State>;
+type MutatingAction<State> = (state: State) => void;
 export type Selector<State, Result> = (state: State) => Result;
 export type Listener = () => void;
-export type SetState<State> = (updater: Updater<State>) => void;
-export type Initializer<State> = (setState: SetState<State>) => State;
+export type SetState<State> = (nonMutatingAction: NonMutatingAction<State>) => void;
+export type UpdateState<State> = (mutatingAction: MutatingAction<State>) => void;
+export type Initializer<State> = (setState: SetState<State>, updateState: UpdateState<State>) => State;
 export type DebugFn<State> = (next: State, prev?: State, updated?: Partial<State>) => void;
