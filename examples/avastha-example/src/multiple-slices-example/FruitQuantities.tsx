1dc5660f2a55ee0bf1777ac79e8baddc061fa4de
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-04-09 01:59:49 +0530

    fix: support new store structure in multi slice example

--- examples/avastha-example/src/multiple-slices-example/FruitQuantities.tsx
+++ examples/avastha-example/src/multiple-slices-example/FruitQuantities.tsx
@@ -1,15 +1,15 @@
-import useFruitStore from "./FruitStore"
+import fruitStore from './FruitStore';
 
 export default function FruitQuantities() {
-    const { appleQty, orangeQty } = useFruitStore((state) => ({
-        appleQty: state.apple.quantity,
-        orangeQty: state.orange.quantity
-    }))
-    return (
-        <p>
-            <span>Apples: {appleQty}</span>
-            <br />
-            <span>Oranges: {orangeQty}</span>
-        </p>
-    )
+	const { appleQty, orangeQty } = fruitStore.useState((state) => ({
+		appleQty: state.apple.quantity,
+		orangeQty: state.orange.quantity,
+	}));
+	return (
+		<p>
+			<span>Apples: {appleQty}</span>
+			<br />
+			<span>Oranges: {orangeQty}</span>
+		</p>
+	);
 }
